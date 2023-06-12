74fd5e4ccfd933a1ac64e65cf4d7947aca1f191d
LuisEAL0 <zluisalvaa@gmail.com>
2023-06-12 15:02:16 -0600

    Add outline of "About Me" page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profile_image from '../assets/profile_pic_name.jpg';
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src = {profile_image}
+                alt="Profile Pic"
+                ></img>
+            </div>
+          </div>
+
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title"> Luis Alvarado-Labarca </div>
+              <div className="brief_description">
+                It's a me
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     )
   }
